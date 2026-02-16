6243385f396c6d97c71e9b68f7af981c098cb856
SamruddhiSShetty <shettysamrudhi12@gmail.com>
2026-02-16 11:45:18 +0530

    Updated UI and added components

---
+++ JSXRules/src/components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <nav>
+      <img
+        className="img"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbAmafdPNr9fd0KC0Z98WYEC7Wl1wYlPVf-A&s"
+        alt="not found"
+      />
+      <h1 className="h1n">Rules of JSX</h1>
+    </nav>
+  );
+}
---
+++ JSXRules/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+export default function ManinContent() {
+  return (
+    <ul className="list">
+      <li>Must enclose within a Parent or Fragment</li>
+      <li>Close the tags properly</li>
+      <li>Use className for CSS class,as class is reserved word in JS</li>
+      <li>Use camelCase for attributes</li>
+      <li>Use flower braces for JS</li>
+    </ul>
+  );
+}
